cfbded1fb226810ef4ecaee7c22295192d7e36a9
thecfoguy <thecfoguy@gmail.com>
2026-09-10 07:57:09 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,15 @@
 
+const CACHE = "hunch-v4";
+const FILES = ["./", "index.html", "manifest.json", "icon-192.png", "icon-512.png"];
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
+  self.skipWaiting();
+});
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))
+  ).then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});

e7460e20491646e545d67ef0060ef3bfafde06ad
thecfoguy <thecfoguy@gmail.com>
2026-09-20 10:36:44 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,51 @@
+const CACHE_NAME = "notable-cache-v1";
 
-const CACHE = "hunch-v4";
-const FILES = ["./", "index.html", "manifest.json", "icon-192.png", "icon-512.png"];
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
 self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
   self.skipWaiting();
 });
+
 self.addEventListener("activate", event => {
-  event.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))
-  ).then(() => self.clients.claim()));
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
+  );
+
+  self.clients.claim();
 });
+
 self.addEventListener("fetch", event => {
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, copy);
+        });
+
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
